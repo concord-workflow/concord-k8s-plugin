9494bcd8786e7b024ac2459089f86776053d8fdb
Jason van Zyl <jason@vanzyl.ca>
2020-01-08 11:04:05 -0500

    Adding directory task
    - help with the new feature where a pre/post extension point has been added where we
      scan directories to pick up manifests that need to be installed.

---
+++ src/main/java/com/walmartlabs/concord/plugins/directory/DirectoryTask.java
@@ -0,0 +1,23 @@
+package com.walmartlabs.concord.plugins.directory;
+
+import ca.vanzyl.concord.plugins.TaskSupport;
+
+import javax.inject.Named;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+import static com.walmartlabs.concord.plugins.directory.DirectoryUtils.scanDirectory;
+
+@Named("directoryTool")
+public class DirectoryTask extends TaskSupport {
+
+    public boolean exists(String directoryName) {
+        return Files.exists(Paths.get(directoryName));
+    }
+
+    public List<String> scan(String directoryName) throws IOException {
+        return scanDirectory(directoryName);
+    }
+}
---
+++ src/main/java/com/walmartlabs/concord/plugins/directory/DirectoryUtils.java
@@ -0,0 +1,23 @@
+package com.walmartlabs.concord.plugins.directory;
+
+import org.apache.commons.compress.utils.Lists;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class DirectoryUtils {
+
+    public static List<String> scanDirectory(String directoryName) throws IOException {
+        List<String> files = Lists.newArrayList();
+        Path directory = Paths.get(directoryName);
+        if (Files.exists(directory)) {
+            Files.walk(directory)
+                    .filter(path -> Files.isRegularFile(path))
+                    .forEach(path -> files.add(path.toString()));
+        }
+        return files;
+    }
+}
