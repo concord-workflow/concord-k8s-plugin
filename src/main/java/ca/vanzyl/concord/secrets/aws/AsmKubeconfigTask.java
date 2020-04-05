0ceae46d394d9a99adf81ad5bda4bfd6b0209061
ajayk <ajaykemparaj@gmail.com>
2020-04-04 22:57:46 -0700

    Add sessionToken support for ASmKubeConfigTask

--- src/main/java/ca/vanzyl/concord/secrets/aws/AsmKubeconfigTask.java
+++ src/main/java/ca/vanzyl/concord/secrets/aws/AsmKubeconfigTask.java
@@ -1,9 +1,9 @@
 package ca.vanzyl.concord.secrets.aws;
 
 import ca.vanzyl.concord.plugins.TaskSupport;
+import ca.vanzyl.concord.plugins.secrets.ConcordSecretsClient;
 import com.amazonaws.services.secretsmanager.model.ResourceExistsException;
 import com.walmartlabs.concord.client.ApiClientFactory;
-import ca.vanzyl.concord.plugins.secrets.ConcordSecretsClient;
 import com.walmartlabs.concord.sdk.Context;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -32,15 +32,27 @@ public class AsmKubeconfigTask extends TaskSupport {
         String kubeconfigName = varAsString(context, "kubeconfigName");
         String awsAccessKey = varAsString(context, "awsAccessKey");
         String awsSecretKey = varAsString(context, "awsSecretKey");
+        String awsSessionToken = varAsString(context, "awsSessionToken");
         ConcordSecretsClient concordSecretsClient = new ConcordSecretsClient(apiClient(apiClientFactory, context));
         String kubeconfigContent = concordSecretsClient.secretAsString(orgName(context), kubeconfigName);
 
         List<String> regions = (List<String>) context.getVariable("regions");
         for (String region : regions) {
             logger.info("Attempting to write {} to ASM in {}.", kubeconfigName, region);
-            AsmClient asmClient = new AsmClient(region, awsAccessKey, awsSecretKey);
+
+            AsmClient asmClient;
+            if (awsSessionToken != null && awsSessionToken != "") {
+                asmClient = new AsmClient(region, awsAccessKey, awsSecretKey, awsSessionToken);
+            } else {
+                asmClient = new AsmClient(region, awsAccessKey, awsSecretKey);
+            }
             try {
-                asmClient.put(kubeconfigName, kubeconfigContent);
+                if (asmClient.get(kubeconfigName) != null && asmClient.get(kubeconfigName) != "") {
+                    logger.warn("The secret {} already exists in {} and won't be overridden.", kubeconfigName, region);
+                    return;
+                } else {
+                    asmClient.put(kubeconfigName, kubeconfigContent);
+                }
                 logger.info("The secret {} successfully written to {}.", kubeconfigName, region);
             } catch (ResourceExistsException e) {
                 logger.warn("The secret {} already exists in {} and won't be overridden.", kubeconfigName, region);
