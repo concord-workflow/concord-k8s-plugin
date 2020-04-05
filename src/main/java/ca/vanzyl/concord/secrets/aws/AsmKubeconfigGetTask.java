4484a56819a6f9098536bc1a4a5de4616c4ac640
ajayk <ajaykemparaj@gmail.com>
2020-04-04 22:08:07 -0700

    add support for aws session tokens

--- src/main/java/ca/vanzyl/concord/secrets/aws/AsmKubeconfigGetTask.java
+++ src/main/java/ca/vanzyl/concord/secrets/aws/AsmKubeconfigGetTask.java
@@ -1,8 +1,8 @@
 package ca.vanzyl.concord.secrets.aws;
 
-import com.walmartlabs.concord.client.ApiClientFactory;
 import ca.vanzyl.concord.plugins.k8s.K8sTaskSupport;
 import ca.vanzyl.concord.plugins.secrets.ConcordSecretsClient;
+import com.walmartlabs.concord.client.ApiClientFactory;
 import com.walmartlabs.concord.sdk.Context;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -27,27 +27,34 @@ public class AsmKubeconfigGetTask extends K8sTaskSupport {
     @Override
     public void execute(Context context) throws Exception {
 
-        if(!clusterExists(context)) {
+        if (!clusterExists(context)) {
             return;
         }
 
         String kubeconfigName = varAsString(context, "kubeconfigName");
         String awsAccessKey = varAsString(context, "awsAccessKey");
         String awsSecretKey = varAsString(context, "awsSecretKey");
+        String awsSessionToken = varAsString(context, "awsSessionToken");
         ConcordSecretsClient concordSecretsClient = new ConcordSecretsClient(apiClient(apiClientFactory, context));
 
         List<String> regions = (List<String>) context.getVariable("regions");
         for (String region : regions) {
             logger.info("Attempting to get {} from ASM in {}.", kubeconfigName, region);
             try {
-                AsmClient asmClient = new AsmClient(region, awsAccessKey, awsSecretKey);
+                AsmClient asmClient;
+                if (awsSessionToken != null && awsSessionToken != "") {
+                    asmClient = new AsmClient(region, awsAccessKey, awsSecretKey, awsSessionToken);
+                } else {
+                    asmClient = new AsmClient(region, awsAccessKey, awsSecretKey);
+                }
+
                 String kubeconfigContent = asmClient.get(kubeconfigName);
-                if(concordSecretsClient.binaryDataSecret(orgName(context), kubeconfigName) == null) {
+                if (concordSecretsClient.binaryDataSecret(orgName(context), kubeconfigName) == null) {
                     concordSecretsClient.addPlainSecret(orgName(context), kubeconfigName, false, null, kubeconfigContent.getBytes());
                 }
                 logger.info("Kubeconfig '{}' successfully transferred from {} to Concord's secret store.", kubeconfigName, region);
                 return;
-            } catch(Exception e) {
+            } catch (Exception e) {
                 logger.info("Kubeconfig '{}' not found in  {}.", kubeconfigName, region);
             }
         }
