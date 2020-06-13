1aa7a4186e122e92aca83352499c24d3d0512ad7
Jason van Zyl <jason@vanzyl.ca>
2020-06-13 16:54:24 -0400

    Account for an odd issue messing up the secrets map

--- src/main/java/ca/vanzyl/concord/plugins/k8s/initialize/K8sValidateTask.java
+++ src/main/java/ca/vanzyl/concord/plugins/k8s/initialize/K8sValidateTask.java
@@ -1,15 +1,22 @@
 package ca.vanzyl.concord.plugins.k8s.initialize;
 
-import com.walmartlabs.concord.client.ApiClientFactory;
 import ca.vanzyl.concord.plugins.k8s.K8sTaskSupport;
+import ca.vanzyl.concord.secrets.aws.AsmKubeconfigGetTask;
+import com.walmartlabs.concord.client.ApiClientFactory;
 import com.walmartlabs.concord.sdk.Context;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import javax.inject.Named;
+
+import java.util.List;
 import java.util.Map;
 
 @Named("k8sValidate")
 public class K8sValidateTask extends K8sTaskSupport {
+
+    private final static Logger logger = LoggerFactory.getLogger(K8sValidateTask.class);
 
     @Inject
     public K8sValidateTask(ApiClientFactory apiClientFactory) {
@@ -19,10 +26,11 @@ public class K8sValidateTask extends K8sTaskSupport {
     @Override
     public void execute(Context context) throws Exception {
 
-        // Completely validate the clusterRequest
-        Map<String, Object> clusterRequest = clusterRequest(context);
-
         // Completely validate all necessary secrets
-        Map<String, String> secrets = (Map<String, String>) context.getVariable("secrets");
+        Object secrets = context.getVariable("secrets");
+        if (secrets instanceof List) {
+            logger.warn("Forcing coercion of secrets map!");
+            context.setVariable("secrets", ((List)secrets).get(0));
+        }
     }
 }
